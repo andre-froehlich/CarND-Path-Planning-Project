488ea85df74ee246f89012f71efabcb061a69163
AFR <business@xn--andre-frhlich-pmb.de>
2017-09-08 16:01:01 +0200

    .

--- Xcode/Path_Planning.build/Debug/path_planning.build/Objects-normal/x86_64/car.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/car.cpp \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/helper.hpp \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/car.hpp \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/position.hpp
--- Xcode/Path_Planning.build/Debug/path_planning.build/Objects-normal/x86_64/position.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/position.cpp \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/position.hpp \
-  /Users/business/Udacity/CarND-Path-Planning-Project/src/helper.hpp
